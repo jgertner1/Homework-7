b0c221e1f31d068f44c3ff8cb8feb4998575c90b
jacksongertner <jgertner@umich.edu>
2023-04-08 10:49:07 -0400

    four functions

--- HW7.py
+++ HW7.py
@@ -119,7 +119,11 @@ def birthyear_nationality_search(age, country, cur, conn):
     # HINT: You'll have to use JOIN for this task.
 
 def position_birth_search(position, age, cur, conn):
-       pass
+    cur.execute("SELECT Players.name, Positions.position, Players.birthyear FROM Players JOIN Positions ON Players.position_id = Positions.id WHERE Positions.position = ? AND Players.birthyear > ?", (position, (2023 - age)))
+    lst = cur.fetchall()
+    conn.commit()
+    return lst
+
 
 
 # [EXTRA CREDIT]
